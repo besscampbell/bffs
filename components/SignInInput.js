edb8b2a4c05f7868c9940280eee209998324f18c
Bess Campbell <bess.k.campbell@gmail.com>
2021-03-09 14:42:56 -0800

    Confirm login alert

--- components/SignInInput.js
+++ components/SignInInput.js
@@ -1,41 +1,53 @@
 import React, {useState} from 'react';
-import {TextInput, View, Button, Text} from 'react-native';
+import {TextInput, View, Button, Text, Alert} from 'react-native';
 import firebase, { auth } from 'firebase/app';
+import 'firebase/auth';
 
 
-const SignInInput = (props) => {
+const SignInInput = ({moveTo}) => {
   const [email, setEmail]= useState('');
   const [password, setPassword]= useState('');
-  const handleEmailInput = (email) => {
-    setEmail(email);
-  }
-  const handlePasswordInput = (password) => {
-    setPassword(password);
-  }
+  const [fetching, setFetching] = useState(false);
+  const [error, setError] = useState('');
+  const [isValid, setValid] = useState(true);
 
-  // function doSignUp () {
-  //   if(!email){
-  //     setError("Email required *");
-  //     setValid(false);
-  //     return;
-  //   } else if (!password && password.trim() && password.length > 6){
-  //     setError("Weak password, minimum 5 chars");
-  //     setValid(false);
-  //     return;
-  //   } else if (!isValidEmail(email)){
-  //     setError("Invalid Email");
-  //     setValid(false);
-  //     return;
-  //   }
 
-  //   doCreateUser(email, password);
-  // }
+  function doSignUp () {
+    if(!email){
+      setError("Email required *");
+      setValid(false);
+      return;
+    } else if (!password && password.trim() && password.length > 6){
+      setError("Weak password, minimum 5 chars");
+      setValid(false);
+      return;
+    } else if (!isValid){
+      setError("Invalid Email");
+      setValid(false);
+      return;
+    }
+
+    doCreateUser(email, password);
+  }
 
   const doCreateUser = async (email, password) => {
     try{
       let feedback = await auth().createUserWithEmailAndPassword(email, password)
-      if(feedback){
-        console.log("Success!", feedback)
+      if(feedback && feedback.user){
+        Alert.alert(
+          "Success ✅",
+          `Account created for ${email}`,
+          [
+            // {
+            //   text: 'Invite your bestie',
+            //   onPress:() => navigation.navigate('Home')
+            // },
+            {
+              text: 'Get Started',
+              onPress: () => moveTo.navigate('Home'),
+            }
+          ]
+        );
       }
     } catch(e) {
       console.error(e.message)
@@ -45,23 +57,33 @@ const SignInInput = (props) => {
   return (
     <View>
       <TextInput
+        label={"Email Address"}
         style={styles.input}
         placeholder="Email"
-        onChangeText={handleEmailInput}
+        autoCapitalize={"none"}
+        keyboardType="email-address"
+        onChangeText={text => {
+          setError
+          setEmail(text)
+        }}
+        error={isValid}
         value={email}
         maxLength={35}
       />
       <TextInput
+        label={'Password'}
         style={styles.input}
         placeholder="Password"
-        onChangeText={handlePasswordInput}
+        autoCapitalize={'none'}
+        onChangeText={text =>
+          setPassword(text)}
         value={password}
         maxLength={35}
       />
       <Button
           style={styles.button}
-          title="Sign In"
-          onPress={()=> doCreateUser(email, password)}
+          title="Register"
+          onPress={()=> doSignUp(email, password)}
         />
     </View>
   );
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,39 +3,12 @@ import firebase, { auth } from 'firebase/app';
 import SignInInput from '../components/SignInInput';
 import {View, StyleSheet, Platform, StatusBar} from 'react-native';
 
-const Login = () => {
-  function doSignUp () {
-    if(!email){
-      setError("Email required *");
-      setValid(false);
-      return;
-    } else if (!password && password.trim() && password.length > 6){
-      setError("Weak password, minimum 5 chars");
-      setValid(false);
-      return;
-    } else if (!isValidEmail(email)){
-      setError("Invalid Email");
-      setValid(false);
-      return;
-    }
+const Login = ({navigation}) => {
 
-    doCreateUser(email, password);
-  }
-
-  const doCreateUser = async (email, password) => {
-    try{
-      let feedback = await auth().createUserWithEmailAndPassword(email, password)
-      if(feedback){
-        console.log(tag, "?", feedback)
-      }
-    } catch(e) {
-      console.error(e.message)
-    }
-  }
 
   return(
     <View style={styles.container}>
-      <SignInInput onAddUser={doSignUp}/>
+      <SignInInput moveTo={navigation}/>
     </View>
   );
 }
